1d63f4a238327aaa331c2c5f1c1fa70a8a627495
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-08-20 12:52:22 +0100

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 'use client';
 import "../styles/globals.css"
+import Head from "next/head";
+import img from "../pages/image/Logo.png"
 import {Provider} from "./component/context";
 export default function App({ Component, pageProps }) {
   return(
     <>
+      <Head>
+      <title>Ranchidpro</title>
+        <link rel="icon" href="https://ik.imagekit.io/9nikkw38wtz/Logo_bH0DiEMPV.png?updatedAt=1724154495876" />
+      </Head>
     <Provider>
     <Component {...pageProps} />
     </Provider>
